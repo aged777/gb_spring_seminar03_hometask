2f839ce68ebe1b8d167479c19f176c3ea2eee4c5
Aleksandr Volkov <222volkov@gmail.com>
2024-05-07 20:35:00 +0300

    To make the base part of the hometask

--- src/main/java/ru/gb/spring/seminar03/example3_sem3/controllers/TaskController.java
+++ src/main/java/ru/gb/spring/seminar03/example3_sem3/controllers/TaskController.java
@@ -2,6 +2,7 @@ package ru.gb.spring.seminar03.example3_sem3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gb.spring.seminar03.example3_sem3.domain.User;
 import ru.gb.spring.seminar03.example3_sem3.services.DataProcessingService;
@@ -32,9 +33,17 @@ public class TaskController {
 
     // метод filterUsersByAge
     // подсказка @GetMapping("/filter/{age}")
+    @GetMapping("/tasks/filter/{age}") //localhost:8080/tasks/sort
+    public List<User> filterUsersByAge(@PathVariable("age") int age) {
+        return service.getRepository().filterUsersByAge(service.getRepository().getUsers(), age);
+    }
 
     // метод calculateAverageAge
     // подсказка @GetMapping("/calc")
+    @GetMapping("/tasks/calc") //localhost:8080/tasks/calc
+    public double calculateAverageAge() {
+        return service.getRepository().calculateAverageAge(service.getRepository().getUsers());
+    }
 }
 
 
